6e2da7a1c0437b3364f9d1854cf48896b0b35803
AmitaJoshi <amitajoshi1992@gmail.com>
2021-05-03 18:56:05 +0530

    Update setup_env.groovy

--- vars/setup_env.groovy
+++ vars/setup_env.groovy
@@ -1,36 +1,39 @@
 def call(Map pipelineParams) {
   try{
-    timeout(time: 60, unit: 'MINUTES') {
-      env.BRANCH = pipelineParams.BRANCH
-      env.REPO = pipelineParams.REPO
-      env.BUILD_NODE = pipelineParams.BUILD_NODE
-      pipeline {
-       node(pipelineParams.BUILD_NODE)
-	      {
-          stage("Code Checkout") 
+    timeout(time: 60, unit: 'MINUTES')
+	{
+      	env.BRANCH = pipelineParams.BRANCH
+      	env.REPO = pipelineParams.REPO
+      	env.BUILD_NODE = pipelineParams.BUILD_NODE
+      	pipeline 
 		{
-		
-    env.SCM_URL=REPO
-    echo "Code checkout from SCM Repo"
-    sh '''
-  		echo 'workspace cleanup'$PWD
-		rm -rf $WORKSPACE/*
-   		echo 'deleted'
-		git clone --single-branch --branch ${BRANCH} ${SCM_URL}
-    ''' 
-    echo "Checkout is completed!"
-    }
-		     stage("playbook deploy") {
-           sh '''
-             ansible-playbook playbook.yml -i inventory.txt -e "ENV=$BUILD_NODE"
-              '''
-         }
-        }
+       		node(pipelineParams.BUILD_NODE)
+	      	{
+          		stage("Code Checkout") 
+			{
+    			env.SCM_URL=REPO
+    			echo "Code checkout from SCM Repo"
+    			sh '''
+  				echo 'workspace cleanup'$PWD
+				rm -rf $WORKSPACE/*
+   				echo 'deleted'
+				git clone --single-branch --branch ${BRANCH} ${SCM_URL}
+    			''' 
+    			echo "Checkout is completed!"
+    			}
+		     	stage("playbook deploy") 
+			{
+           		sh '''
+             		ansible-playbook playbook.yml -i inventory.txt -e "ENV=$BUILD_NODE"
+              		'''
+         		}
+        	}
+		}
+		}
     catch (err) {
     echo "in catch block" 
     echo "Caught: ${err}" 
     currentBuild.result = 'FAILURE' 
-    
     throw err
   }
 }
