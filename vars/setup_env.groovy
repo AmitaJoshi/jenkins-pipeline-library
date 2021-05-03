0b96b1ea59479a1dbacc666e83493f4992124284
AmitaJoshi <amitajoshi1992@gmail.com>
2021-05-03 19:06:45 +0530

    Update setup_env.groovy

--- vars/setup_env.groovy
+++ vars/setup_env.groovy
@@ -5,25 +5,27 @@ def call(Map pipelineParams) {
       	env.BRANCH = pipelineParams.BRANCH
       	env.REPO = pipelineParams.REPO
       	env.BUILD_NODE = pipelineParams.BUILD_NODE
+	env.GIT_GROUP = pipelineParams.GIT_GROUP
       	pipeline 
 		{
        		node(pipelineParams.BUILD_NODE)
 	      	{
           		stage("Code Checkout") 
 			{
-    			env.SCM_URL=REPO
+    			
     			echo "Code checkout from SCM Repo"
     			sh '''
   				echo 'workspace cleanup'$PWD
 				rm -rf $WORKSPACE/*
    				echo 'deleted'
-				git clone --single-branch --branch ${BRANCH} ${SCM_URL}
+				git clone --single-branch --branch ${BRANCH} ${GIT_GROUP}${REPO}.git
     			''' 
     			echo "Checkout is completed!"
     			}
 		     	stage("playbook deploy") 
 			{
            		sh '''
+			cd ${REPO}
              		ansible-playbook playbook.yml -i inventory.txt -e "ENV=$BUILD_NODE"
               		'''
          		}
