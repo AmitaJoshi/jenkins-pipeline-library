6eef348ea9f656fba0056a70b59120701a9bbe7f
AmitaJoshi <amitajoshi1992@gmail.com>
2021-05-08 13:10:38 +0530

    Update build_and_deployment_pipeline.groovy

--- vars/build_and_deployment_pipeline.groovy
+++ vars/build_and_deployment_pipeline.groovy
@@ -3,38 +3,40 @@ def call(Map pipelineParams) {
     timeout(time: 60, unit: 'MINUTES') {
       env.BRANCH = pipelineParams.BRANCH
       env.REPO = pipelineParams.REPO
-      pipeline {
-       
+      pipeline
+       {
         node(pipelineParams.BUILD_NODE)
-	      {
+	    {
           stage("Code Checkout") 
-		{
-		
-          env.SCM_URL=REPO
-    echo "Code checkout from SCM Repo"
-    sh '''
-  		echo 'workspace cleanup'$PWD
-		rm -rf $WORKSPACE/*
-   		echo 'deleted'
-		git clone --single-branch --branch ${BRANCH} ${SCM_URL}
-    ''' 
-    echo "Checkout is completed!"
-    }
-		     stage("Build") {
+	        {
+		        env.SCM_URL=REPO
+                echo "Code checkout from SCM Repo"
+                sh '''
+  	        	echo 'workspace cleanup'$PWD
+		        rm -rf $WORKSPACE/*
+   		        echo 'deleted'
+		        git clone --single-branch --branch ${BRANCH} ${SCM_URL}
+                ''' 
+                echo "Checkout is completed!"
+            }
+		stage("Build") {
            sh '''
             cd usermanagement_javasqlproject
             mvn clean install
         '''
-          }
+        }
+        stage("War Deployment"){
+            sh '''
+            cp $WORKSPACE/build_and_deployment_pipeline/usermanagement_javasqlproject/target/*.war /root/tomcat/webapps
+            echo 'war deployed successfully'
+        }
 	}
-      
-      }
+        }
 	    post{
 		    always{
 		    cleanWs()
-		    }
-	    }
-	    
+		          }
+	        }    
     }
   }
    
