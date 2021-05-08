b461302e50fb1e19e358ab7e933a26dbdfc236f7
AmitaJoshi <amitajoshi1992@gmail.com>
2021-05-08 14:39:09 +0530

    Update build_and_deployment_pipeline.groovy

--- vars/build_and_deployment_pipeline.groovy
+++ vars/build_and_deployment_pipeline.groovy
@@ -30,6 +30,8 @@ def call(Map pipelineParams) {
 	    #sh /root/tomcat/bin/shutdown.sh 
 	    sleep 10
             cp $WORKSPACE/usermanagement_javasqlproject/target/*.jar /root/tomcat/webapps
+	    cd /root/tomcat/webapps
+	    chmod 755 *.jar
 	    '''
 	    sh '''
 	    	/root/tomcat/bin/startup.sh
