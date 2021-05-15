616cce823773e89a07bc3012d7281e2d9f1f47fd
minaxi joshi <minaxijoshi3101.com>
2021-05-15 18:12:32 +0530

    declared env variabled in build.groovy file

--- src/com/self/pipeline/springboot/build.groovy
+++ src/com/self/pipeline/springboot/build.groovy
@@ -3,6 +3,8 @@ package com.self.pipeline.springboot
 def call(Map pipelineParams) 
 {
 env.REPO = pipelineParams.REPO
+env.DOCKER_HOST = pipelineParams.DOCKER_HOST
+env.DOCKER_REGISTRY = pipelineParams.DOCKER_REGISTRY
 sh '''
         echo " build a docker image"
         mvn deploy -P docker -Ddocker.host=${DOCKER_HOST} -Ddocker.registry.name=${DOCKER_REGISTRY}
