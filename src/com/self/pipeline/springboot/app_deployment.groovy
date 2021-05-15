e61a8812244041ce7efac78b10995e00b31f14ba
minaxi joshi <minaxijoshi3101.com>
2021-05-15 21:11:47 +0530

    removed / from run command

--- src/com/self/pipeline/springboot/app_deployment.groovy
+++ src/com/self/pipeline/springboot/app_deployment.groovy
@@ -5,6 +5,6 @@ def call(Map pipelineParams)
     env.DOCKER_REGISTRY = pipelineParams.DOCKER_REGISTRY
     env.APP_NAME = pipelineParams.APP_NAME
 	sh '''
-  	    docker run -d --name $APP_NAME -p 8082:8080 $DOCKER_REGISTRY/$APP_NAME 
+  	    docker run -d --name $APP_NAME -p 8082:8080 $DOCKER_REGISTRY$APP_NAME 
     ''' 
 }
