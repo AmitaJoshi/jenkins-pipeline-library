7b2c00f930d9f53c9a74ba785f8a2f3c1d3d5311
minaxi joshi <minaxijoshi3101.com>
2021-05-15 20:42:19 +0530

    removed bin from JAVA_HOME path

--- src/com/self/pipeline/springboot/build.groovy
+++ src/com/self/pipeline/springboot/build.groovy
@@ -6,7 +6,7 @@ env.REPO = pipelineParams.REPO
 env.DOCKER_HOST = pipelineParams.DOCKER_HOST
 env.DOCKER_REGISTRY = pipelineParams.DOCKER_REGISTRY
 sh '''
-        export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-11.0.11.0.9-0.el8_3.x86_64/bin"
+        export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-11.0.11.0.9-0.el8_3.x86_64"
         echo " build a docker image"
         cd $REPO
         mvn deploy -P docker -Ddocker.host=${DOCKER_HOST} -Ddocker.registry.name=${DOCKER_REGISTRY}
