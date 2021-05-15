b7e74265990965ab019913c518a4b0f4dc1819c7
amita joshi <amitajoshi1992@gmail.com>
2021-05-15 14:04:17 +0530

    removed $PWD from chekoutSCM

--- src/com/self/pipeline/util/checkoutSCM.groovy
+++ src/com/self/pipeline/util/checkoutSCM.groovy
@@ -5,10 +5,10 @@ def call(Map pipelineParams)
     env.SCM_URL=pipelineParams.GIT_URL+"/"+pipelineParams.GIT_GROUP+"/"+pipelineParams.REPO+".git"
     env.BRANCH=pipelineParams.BRANCH
     sh
-    """
-        echo 'workspace cleanup'$PWD
+    '''
+        echo 'workspace cleanup'
         rm -rf $WORKSPACE/*
         echo 'deleted'
         git clone --single-branch --branch${BRANCH} ${SCM_URL}
-    """
+    '''
 }
