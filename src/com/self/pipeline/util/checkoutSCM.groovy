6590d04a9785ef2fdd769291d74cc893a8c4df81
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2021-05-15 14:30:20 +0530

    Update checkoutSCM.groovy

--- src/com/self/pipeline/util/checkoutSCM.groovy
+++ src/com/self/pipeline/util/checkoutSCM.groovy
@@ -4,11 +4,13 @@ def call(Map pipelineParams)
 {
 env.SCM_URL= pipelineParams.GIT_URL+"/"+pipelineParams.GIT_GROUP+"/"+pipelineParams.REPO+".git"
 env.BRANCH = pipelineParams.BRANCH
+echo "Code checkout from SCM Repo"
 sh 
     '''
   	        	echo 'workspace cleanup'$PWD
-		        rm -rf $WORKSPACE/*
+		        rm -rf $REPO/*
    		        echo 'deleted'
 		        git clone --single-branch --branch ${BRANCH} ${SCM_URL}
     ''' 
+	 echo "Checkout is completed!"
 }
