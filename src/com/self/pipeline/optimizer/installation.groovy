c1c74dfd5a1a97e3e061b9fe0be08f793bc112b6
amita joshi <amitajoshi1992@gmail.com>
2021-05-15 13:38:43 +0530

    changes

--- src/com/self/pipeline/optimizer/installation.groovy
+++ src/com/self/pipeline/optimizer/installation.groovy
@@ -4,8 +4,8 @@ def call(Map pipelineParams)
 {
 env.ANSIBLE_REPO = pipelineParams.ANSIBLE_REPO
 sh 
-    '''
+    """
   	    cd $ANSIBLE_REPO
         ansible-playbook playbook.yml -i inventory.txt
-    ''' 
+    """ 
 }  
