d6c9c258b904125910a2731f9693dd033050f6ef
sumitgit01 <sumitjoshi1988@gmail.com>
2021-05-16 00:46:13 +0530

    Changes in build and deployment

--- vars/build_and_deployment_pipeline.groovy
+++ vars/build_and_deployment_pipeline.groovy
@@ -1,72 +1,25 @@
 import com.self.pipeline.util.checkoutSCM;
 import com.self.pipeline.optimizer.*;
 
-def call(Map pipelineParams) {
-  try{
-    timeout(time: 60, unit: 'MINUTES') {
-      env.BRANCH = pipelineParams.BRANCH
-
+def call(Map pipelineParams) 
+{
       pipeline
       {
-        if (pipelineParams.DEPLOYMENT_TYPE == "INSTALL_AND_DEPLOY")
-        {
-          SKIP_INSTALL = false
-        }
-        else if(pipelineParams.DEPLOYMENT_TYPE == "INSTALL")
-        {
-          SKIP_INSTALL = false
-
-        }
-        else
-        {
-          SKIP_INSTALL = true
-        }
         node (pipelineParams.BUILD_NODE)
         {
-          stage("install and configure")
-          {
-            if (!SKIP_INSTALL)
-            {
-              echo "checkout ansible"
-              pipelineParams.put('GIT_GROUP',pipelineParams.ANSIBLE_GIT_GROUP)
-              pipelineParams.put('BRANCH',pipelineParams.ANSIBLE_BRANCH)
-              pipelineParams.put('REPO',pipelineParams.ANSIBLE_REPO)
-              new checkoutSCM().call(pipelineParams)
-              new installation().call(pipelineParams)
-            }
-          }
           stage("Code Checkout") 
 	        {
-		        pipelineParams.put('GIT_GROUP',pipelineParams.GIT_GROUP)
-            pipelineParams.put('BRANCH',pipelineParams.BRANCH)
-            pipelineParams.put('REPO',pipelineParams.REPO)
-            new checkoutSCM().call(pipelineParams)
+		        echo 'workspace cleanup'
+            sh '''
+            rm -rf $WORKSPACE/*
+            echo 'deleted'
+            git clone https://github.com/minaxijoshi3101/dummy_webapp.git
             echo"checkout is completed!"
+            '''
           }
-		      stage("Build") 
-          {
-            new build().call(pipelineParams)
-            echo("Build is done!")
-          }
-          stage("War Deployment")
-          {
-            new app_deployment().call(pipelineParams)
-          }
-		      stage("upload artifacts to nexus")
-		      {
-			    echo "artifacts uploaded to nexus server"
-		      }
-	}
         }
 	       
     }
   }
-   
-    catch (err) {
-    echo "in catch block" 
-    echo "Caught: ${err}" 
-    currentBuild.result = 'FAILURE' 
-    
-    throw err
-  }
-}
+  
+
