fbd3f98c8e7b2bd8f961da26b8f14f39c8ba347b
amita joshi <amitajoshi1992@gmail.com>
2021-05-15 09:58:32 +0530

    structure commiting

---
+++ src/com/self/pipeline/util/checkoutSCM.groovy
@@ -0,0 +1,14 @@
+package com.self.pipeline.util
+
+def call(Map pipelineParams)
+{
+    env.SCM_URL=pipelineParams.GIT_URL+"/"pipelineParams.GIT_GROUP+"/"pipelineParams.REPO".git"
+    env.BRANCH=pipelineParams.BRANCH
+    sh
+    '''
+        echo 'workspace cleanup'$PWD
+        rm -rf $WORKSPACE/*
+        echo 'deleted'
+        git clone --single-branch --branch${BRANCH} ${SCM_URL}
+    '''
+}
