bfb60098b03e1369608aa76694d5bf2e5ba30beb
minaxi joshi <minaxijoshi3101.com>
2021-05-15 21:09:31 +0530

    added docker run command in deployment file

--- src/com/self/pipeline/springboot/app_deployment.groovy
+++ src/com/self/pipeline/springboot/app_deployment.groovy
@@ -2,7 +2,9 @@ package com.self.pipeline.springboot
 
 def call(Map pipelineParams) 
 {
+    env.DOCKER_REGISTRY = pipelineParams.DOCKER_REGISTRY
+    env.APP_NAME = pipelineParams.APP_NAME
 	sh '''
-  	    echo 'docker run'       
+  	    docker run -d --name $APP_NAME -p 8082:8080 $DOCKER_REGISTRY/$APP_NAME 
     ''' 
 }
