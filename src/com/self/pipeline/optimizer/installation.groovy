2cc6dd47e40e4dbe0b33e321b552ca2a73b892c4
amita joshi <amitajoshi1992@gmail.com>
2021-05-16 00:46:49 +0530

    added "ENV=QA"

--- src/com/self/pipeline/optimizer/installation.groovy
+++ src/com/self/pipeline/optimizer/installation.groovy
@@ -5,6 +5,6 @@ def call(Map pipelineParams)
 env.ANSIBLE_REPO = pipelineParams.ANSIBLE_REPO
 sh '''
   	    cd $ANSIBLE_REPO
-        ansible-playbook playbook.yml -i inventory.txt
+        ansible-playbook playbook.yml -i inventory.txt -e "ENV=QA"
 ''' 
 }  
