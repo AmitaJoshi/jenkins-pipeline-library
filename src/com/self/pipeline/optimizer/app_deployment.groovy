7c0f335fbeb7802999ed398138d08d4932f205a3
amita joshi <amitajoshi1992@gmail.com>
2021-05-16 00:30:28 +0530

    app_deployent file changed to war

--- src/com/self/pipeline/optimizer/app_deployment.groovy
+++ src/com/self/pipeline/optimizer/app_deployment.groovy
@@ -1,12 +1,13 @@
 package com.self.pipeline.optimizer
 def call(Map pipelineParams)         
 {         
+	env.REPO = pipelineParams.REPO
     sh '''
 	    sh /root/tomcat/bin/shutdown.sh 
 	    sleep 10
-        cp $WORKSPACE/usermanagement_javasqlproject/target/*.jar /root/tomcat/webapps
+        cp $WORKSPACE/$REPO/target/*.war /root/tomcat/webapps
 	    cd /root/tomcat/webapps
-	    chmod 755 *.jar
+	    chmod 755 *.war
 	    export BUILD_ID=dontkillme
 	    /root/tomcat/bin/startup.sh
     '''
