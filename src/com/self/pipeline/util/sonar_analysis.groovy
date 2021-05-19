debca1a31b3fa0d790c74e2a95787e5ca2b1347b
amita joshi <amitajoshi1992@gmail.com>
2021-05-19 14:44:59 +0530

    applying qualitygate

--- src/com/self/pipeline/util/sonar_analysis.groovy
+++ src/com/self/pipeline/util/sonar_analysis.groovy
@@ -2,11 +2,17 @@ package com.self.pipeline.util
 
 def call(Map pipelineParams)
 {
-     env.REPO = pipelineParams.REPO
+    env.REPO = pipelineParams.REPO
     withSonarQubeEnv('sonarQube'){
-              sh '''
-              cd $REPO 
-              mvn sonar:sonar
-              '''
-            }
+            sh '''
+            cd $REPO 
+            mvn sonar:sonar
+            '''
+    }
+    timeout(time: 1, unit: 'HOURS') { // Just in case something goes wrong, pipeline will be killed after a timeout
+        def qualitygate = waitForQualityGate()
+        if (qualitygate.status != "OK") {
+            error "Pipeline aborted due to quality gate failure: ${qualitygate.status}"
+        }
+    }
 }
