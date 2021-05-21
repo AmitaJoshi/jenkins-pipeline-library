87e5fc4599ca58e631277a42e5d3a89c35f2386c
minaxi joshi <minaxijoshi3101.com>
2021-05-21 23:24:36 +0530

    ansible playbook run code added

--- vars/symlink_pipeline.groovy
+++ vars/symlink_pipeline.groovy
@@ -2,8 +2,11 @@ def call(Map pipelineParams)
 {
      pipeline
        {
-           stage("Code Checkout") 
+          stage("Code Checkout") 
 	            {
+                pipelineParams.put('GIT_GROUP',pipelineParams.GIT_GROUP)
+                pipelineParams.put('BRANCH',pipelineParams.BRANCH)
+                pipelineParams.put('REPO',pipelineParams.REPO)
                 env.SCM_URL= pipelineParams.GIT_URL+"/"+pipelineParams.GIT_GROUP+"/"+pipelineParams.REPO+".git"
                 env.BRANCH = pipelineParams.BRANCH
                 echo "Code checkout from SCM Repo"
@@ -30,9 +33,27 @@ def call(Map pipelineParams)
                 cp *.war /root/tomcat/webapps 
                 '''
               }
-              stage("create symlink")
-              {
-                sh ""
+          stage("create symlink")
+            {            
+              echo "checkout ansible"
+              pipelineParams.put('GIT_GROUP',pipelineParams.ANSIBLE_GIT_GROUP)
+              pipelineParams.put('BRANCH',pipelineParams.ANSIBLE_BRANCH)
+              pipelineParams.put('REPO',pipelineParams.ANSIBLE_REPO)
+              env.ANSIBLE_REPO = pipelineParams.ANSIBLE_REPO
+              env.SCM_URL= pipelineParams.GIT_URL+"/"+pipelineParams.GIT_GROUP+"/"+pipelineParams.REPO+".git"
+              env.BRANCH = pipelineParams.BRANCH
+              echo "Code checkout from SCM Repo"
+                sh '''
+                            echo 'workspace cleanup'$PWD
+                            rm -rf $WORKSPACE/*
+                            echo 'deleted'
+                            git clone --single-branch --branch ${BRANCH} ${SCM_URL}
+                    ''' 
+	                  echo "Checkout is completed!"
+                sh '''
+                      cd $ANSIBLE_REPO
+                      ansible-playbook symlink_artifacts_playbook.yml -i inventory.txt
+                  ''' 
               }
         }
 }
