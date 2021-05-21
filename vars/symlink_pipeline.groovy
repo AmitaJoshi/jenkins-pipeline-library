f65a26c5a2dc5ddc9496c793cbd491b30aedcde0
minaxi joshi <minaxijoshi3101.com>
2021-05-21 23:14:02 +0530

    removed playbook from this git repo

--- vars/symlink_pipeline.groovy
+++ vars/symlink_pipeline.groovy
@@ -3,7 +3,7 @@ def call(Map pipelineParams)
      pipeline
        {
            stage("Code Checkout") 
-	        {
+	            {
                 env.SCM_URL= pipelineParams.GIT_URL+"/"+pipelineParams.GIT_GROUP+"/"+pipelineParams.REPO+".git"
                 env.BRANCH = pipelineParams.BRANCH
                 echo "Code checkout from SCM Repo"
@@ -14,18 +14,25 @@ def call(Map pipelineParams)
 		            git clone --single-branch --branch ${BRANCH} ${SCM_URL}
                 ''' 
 	              echo "Checkout is completed!"
-            }
+              }
           stage("build")
-          {
+              {
                env.REPO = pipelineParams.REPO
                sh '''
-               export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-11.0.11.0.9-0.el8_3.x86_64"
-               echo " build a docker image"
                cd $REPO
-               mvn deploy -P docker -Ddocker.host=${DOCKER_HOST} -Ddocker.registry.name=${DOCKER_REGISTRY}
-              ''' 
-          }
-            }
+               mvn clean install
+               ''' 
+              }
+          stage("deploy")
+              {
+                sh '''
+                cd $WORKSPACE/target
+                cp *.war /root/tomcat/webapps 
+                '''
+              }
+              stage("create symlink")
+              {
+                sh ""
+              }
         }
-       }
 }
